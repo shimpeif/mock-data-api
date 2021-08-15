9121dbc3d2f5ddeb2e7bcb870d0a6e6d8fadbbfe
ernest <cafelashowerezweb@gmail.com>
2021-08-15 19:41:45 +0900

    creating Resolver files

---
+++ Schema/Resolvers.js
@@ -0,0 +1,20 @@
+const { users } = require('../MOCK_DATA100.json');
+
+const resolvers = {
+    Query: {
+        getAllUsers() {
+            return users;
+        },
+    },
+
+    Mutation: {
+        createUser(parent, args) {
+            const newUser = args;
+            users.push(newUser);
+            return newUser;
+        }
+    }
+};
+
+module.exports = { resolvers };
+
